dc291e865887241e5f28269fe37eb3d0977d3c65
Omar <omarghedada@gmail.com>
2026-06-19 01:02:21 +0100

    project skeleton? i think

---
+++ io/plot.py
@@ -0,0 +1,20 @@
+from rendering.maps import BaseMap
+from hydrology.contour import ContourField
+from dataclasses import dataclass
+from pathlib import Path
+from typing import TypedDict
+import geopandas as gpd
+import folium
+
+filename="data_set.csv"
+
+@dataclass
+class MapPlotter:
+    """This class use folium to creates maps
+    """
+    base_map:BaseMap
+    location:list[int]
+    countour:ContourField
+
+    def plot(self):
+        pass
